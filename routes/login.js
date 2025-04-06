9ea7ac55a877f0a21e79ac8cf606c86de1a3ed12
amothm80 <amo.1980@gmail.com>
2025-04-06 05:13:34 +0300

    login logic

--- routes/login.js
+++ routes/login.js
@@ -10,44 +10,53 @@ export const loginRouter = express.Router();
 
 // When you visit http://localhost:3000/login, you will see "Login Page"
 loginRouter.get("/login", (req, res, next) => {
+  if (req.session.loginError) {
+    res.locals.error = "Wrong email/password provided";
+    req.session.loginError = "";
+  }
   res.render("login");
 });
 // import '../controller/passport.js';
 
-// loginRouter.post(
-//   "/login",
-//   passport.authenticate("local",{
-//     failureRedirect: "login-failure",
-//     successRedirect: "login-success"
-//   })
-// );
+loginRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "login-failure",
+    successRedirect: "login-success",
+  })
+);
 
-loginRouter.post("/login", function (req, res, next) {
-  passport.authenticate("local", function (err, user, info) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      res.locals.fields = req.body
-      res.locals.error = info.message
-      return res.render('login')
-    }
+// loginRouter.post("/login", function (req, res, next) {
+//   passport.authenticate("local", function (err, user, info) {
+//     if (err) {
+//       return next(err);
+//     }
+//     if (!user) {
+//       res.locals.fields = req.body
+//       res.locals.error = info.message
+//       return res.render('login')
+//     }
 
-    // // NEED TO CALL req.login()!!!
+//     // // NEED TO CALL req.login()!!!
 
-    req.login(user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      return res.redirect("/login-success");
-    });
-  })(req, res, next);
-});
+//     req.login(user, function (err) {
+//       if (err) {
+//         return next(err);
+//       }
+//       return res.redirect("/login-success");
+//     });
+//   })(req, res, next);
+// });
 
 loginRouter.get("/login-failure", (req, res, next) => {
-  res.render("login-failure");
+  req.session.loginError = true;
+  res.redirect("/login");
 });
 loginRouter.get("/login-success", (req, res, next) => {
-  res.locals.name = req.user.name;
-  res.render("login-success");
+  if (req.isAuthenticated()) {
+    res.locals.name = req.user.name;
+    res.render("login-success");
+  } else {
+    res.redirect("/login")
+  }
 });
