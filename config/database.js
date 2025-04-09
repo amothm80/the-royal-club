8c2031ef9942a01e3e6d1f1f734ef85b0138b4af
amothm80 <amo.1980@gmail.com>
2025-04-09 15:39:21 +0300

    Refactor database SSL configuration to use environment variable

--- config/database.js
+++ config/database.js
@@ -3,12 +3,18 @@ import "dotenv/config";
 import pg from "pg";
 
 const { Pool } = pg;
-
+// let ssl = false;
+// console.log(process.env.SSL)
+// console.log(Boolean(process.env.SSL))
+// if (Boolean(process.env.SSL)){
+//   ssl = true
+// }
+// console.log(ssl)
 export const pool = new Pool({
   host: process.env.DBHOST,
   user: process.env.DBUSER,
   database: process.env.DB,
   password: process.env.DBPASSWORD,
   port: process.env.DBPORT,
-  ssl:true,
+  ssl:process.env.SSL === 'true'?true:false,
 });
